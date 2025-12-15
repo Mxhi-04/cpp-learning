8b5a77924e370a6c2d8940077dc34b3065b85697
Mxhi-04 <mahipatni1828@gmail.com>
2025-12-15 15:33:03 +0530

    Printed numbers from 1 to n using while loop

---
+++ print_1_to_n_while.cpp
@@ -0,0 +1,18 @@
+//program to print numbers till 'n' 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
