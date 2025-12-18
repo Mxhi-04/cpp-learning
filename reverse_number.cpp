9f778883c8ddf9b8c7a2ae415ace32ea74432a08
Mxhi-04 <mahipatni1828@gmail.com>
2025-12-18 23:18:53 +0530

    Reversed a number using while loop

---
+++ reverse_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int n;
+
+    cout << "Enter n : " ;
+    cin >> n;
+
+    int rev = 0;
+
+    while( n>0) {
+
+        int digit= n%10;
+        rev= rev*10 + digit;
+        n= n/10;
+    } 
+
+    cout << "reversed number is :" << rev;
+
+    return 0;
+}
