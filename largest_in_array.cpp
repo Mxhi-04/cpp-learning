6b79bdd1770397a7318ffcd88e70c3ea44b9ce94
Mxhi-04 <mahipatni1828@gmail.com>
2025-12-24 18:29:57 +0530

    created a program to find the largest number in arrays

---
+++ largest_in_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int arr[5];
+
+    cout << "Enter five numbers: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Largest number is: " << max;
+
+    return 0;
+}
